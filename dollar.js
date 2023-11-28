fa4c1febbb211d0a18fb3991ae754345e229a68d
IlakiaSelviSaravanan-FSD <150881218+IlakiaSelviSaravanan-FSD@users.noreply.github.com>
2023-11-28 15:23:14 +0530

    Update dollar.js

--- dollar.js
+++ dollar.js
@@ -3,16 +3,9 @@ request.open ("GET","https://restcountries.com/v3.1/all")
 request.send();
 request.onload = function(){
     var result=JSON.parse(request.response);
-    console.log(result);
-
-   for(var i=0;i<result.length;i++){
-
-    var currencies=result[i].currencies;
-    var countries=result[i].name.common;
-    console.log("the currencies"+currencies);
-    console.log("the countrynames"+countries);
-   }
-    
+   var res=result.map((ele)=>ele.currencies);
+   
+   console.log(res);
 }
 
 
